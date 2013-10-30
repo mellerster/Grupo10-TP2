8f3d3402e43ed22f88e16a987ac1170fa939139c
David <david.mdq89@gmail.com>
2013-10-30 14:11:09 -0300

    Saco tests que eran solo de prueba

--- src/main/java/ClientTP2/MyTests.java
+++ src/main/java/ClientTP2/MyTests.java
@@ -16,76 +16,62 @@ public class MyTests extends Testeable {
 	public void ReportCountIncreaseTest() {
 		int beforeResults = Reporter.getReporter().getResults().size();
 		Assert.isTrue(true,"ReportCountIncreaseTest");
-		Assert.AreEquals(beforeResults + 1, Reporter.getReporter().getResults().size(),"ReportCountIncreaseTest");
+		Assert.AreEquals(beforeResults + 1, Reporter.getReporter()
+				.getResults().size(),"ReportCountIncreaseTest");
 	}
 	
-	public void ReportFailCountIncreaseTest(){
+	public void ReportFailCountIncreaseTest() {
 		int beforeResults = Reporter.getReporter().getFailures().size();
 		Assert.isTrue(false,"ReportFailCountIncreaseTest");
-		Assert.AreEquals(beforeResults + 1, Reporter.getReporter().getFailures().size(),"ReportFailCountIncreaseTest");
+		Assert.AreEquals(beforeResults + 1, Reporter.getReporter()
+				.getFailures().size(),"ReportFailCountIncreaseTest");
 	}
 	
-	public void ReportFailCountNotIncreaseTest(){
+	public void ReportFailCountNotIncreaseTest() {
 		int beforeResults = Reporter.getReporter().getFailures().size();
 		Assert.isTrue(true,"ReportFailCountNotIncreaseTest");
-		Assert.AreEquals(beforeResults, Reporter.getReporter().getFailures().size(),"ReportFailCountNotIncreaseTest");
+		Assert.AreEquals(beforeResults, Reporter.getReporter()
+				.getFailures().size(),"ReportFailCountNotIncreaseTest");
 	}
 	
-	public void ResultOkStateTest(){
+	public void ResultOkStateTest() {
 		Result result = new ResultOk("ResultOkStateTest");
 		Assert.AreEquals(true, result.getState(), "ResultOkStateTest");
 	}
 	
-	public void ResultFailStateTest(){
+	public void ResultFailStateTest() {
 		Result result = new ResultFail("ResultFailStateTest");
 		Assert.AreEquals(false, result.getState(), "ResultFailStateTest");
 	}
 	
-	public void AssertIsTrueSuccessfullResultTest(){
+	public void AssertIsTrueSuccessfullResultTest() {
 		Assert.isTrue(true, "AssertIsTrueSuccessfullResultTest");
 		Assert.AreEquals(true, Reporter.getReporter().getResults()
 				.get(Reporter.getReporter().getResults().size()-1).
 				wasSuccessfull(), "AssertIsTrueSuccessfullResultTest");
 	}
 	
-	public void AssertIsTrueNotSuccessfullResultTest(){
+	public void AssertIsTrueNotSuccessfullResultTest() {
 		Assert.isTrue(false, "AssertIsTrueNotSuccessfullResultTest");
 		Assert.AreEquals(false, Reporter.getReporter().getResults()
 				.get(Reporter.getReporter().getResults().size()-1)
 				.wasSuccessfull(), "AssertIsTrueNotSuccessfullResultTest");
 	}
 	
-	public void AssertEqualsSuccessfullResultTest(){
+	public void AssertEqualsSuccessfullResultTest() {
 		Assert.AreEquals(1, 1, "AssertEqualsSuccessfullResultTest");
 		Assert.AreEquals(true, Reporter.getReporter().getResults()
 				.get(Reporter.getReporter().getResults().size()-1)
 				.wasSuccessfull(), "AssertEqualsSuccessfullResultTest");
 	}
 	
-	public void AssertEqualsNotSuccessfullResultTest(){
+	public void AssertEqualsNotSuccessfullResultTest() {
 		Assert.AreEquals(1, 2, "AssertEqualsNotSuccessfullResultTest");
 		Assert.AreEquals(false, Reporter.getReporter().getResults()
 				.get(Reporter.getReporter().getResults().size()-1)
 				.wasSuccessfull(), "AssertEqualsNotSuccessfullResultTest");
 	}
 	
-/*
-	public void test1() {
-		Assert.isTrue(true,"Test 1");
-	}
-	
-	public void test2() {
-		Assert.isTrue(false, "Test 2");
-	}
-	
-	public void test3(){
-		Assert.AreEquals(1, 1,"Test 3");
-	}
-	
-	public void test4(){
-		Assert.AreEquals(1, 2, "Test 4");
-	}
-*/
 	public void init() {
 		super.addTest(new Test() {
 			public void run() {
@@ -132,28 +118,6 @@ public class MyTests extends Testeable {
 				AssertEqualsNotSuccessfullResultTest();
 			}
 		});
-/*
-		super.addTest(new Test() {
-			public void run() {
-				test1();
-			}
-		});
-		super.addTest(new Test() {
-			public void run() {
-				test2();
-			}
-		});
-		super.addTest(new Test() {
-			public void run() {
-				test3();
-			}
-		});
-		super.addTest(new Test() {
-			public void run() {
-				test4();
-			}
-		});
-*/
 	}
 	
 }
