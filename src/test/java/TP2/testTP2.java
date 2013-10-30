9a621454516719f692e6c1cbd938091c4b24c9ad
David <david.mdq89@gmail.com>
2013-10-30 10:00:10 -0300

    Agregado de tests

--- src/test/java/TP2/testTP2.java
+++ src/test/java/TP2/testTP2.java
@@ -8,33 +8,53 @@ import static org.junit.Assert.assertEquals;
 public class testTP2 {
 	
 	@Test
-	public void AssertReportCount(){
+	public void AssertReportCountTest(){
 		int beforeResults = Reporter.getReporter().getResults().size();
 		Assert.isTrue(true,"");
 		assertEquals(beforeResults + 1, Reporter.getReporter().getResults().size());
 	}
 	
 	@Test
-	public void testResultOkState(){
-		Result result = new ResultOk("una descripcion");
+	public void AssertReportFailCountTest(){
+		int beforeResults = Reporter.getReporter().getFailures().size();
+		Assert.isTrue(false,"");
+		assertEquals(beforeResults + 1, Reporter.getReporter().getFailures().size());
+	}
+	
+	@Test
+	public void ResultOkStateTest(){
+		Result result = new ResultOk("Description of result ok");
 		assertEquals(true, result.getState());
 	}
 
 	@Test
-	public void testResultFailState(){
-		Result result = new ResultFail("una descripcion");
+	public void ResultFailStateTest(){
+		Result result = new ResultFail("Description of result fail");
 		assertEquals(false, result.getState());
 	}
 	
 	@Test
-	public void AssertAddSuccessfullResult(){
+	public void AssertIsTrueSuccessfullResultTest(){
 		Assert.isTrue(true, "");
 		assertEquals(true, Reporter.getReporter().getResults().get(Reporter.getReporter().getResults().size()-1).wasSuccessfull());
 	}
 	
 	@Test
-	public void AssertAddNotSuccessfullResult(){
+	public void AssertIsTrueNotSuccessfullResultTest(){
 		Assert.isTrue(false, "");
 		assertEquals(false, Reporter.getReporter().getResults().get(Reporter.getReporter().getResults().size()-1).wasSuccessfull());
 	}
+	
+	@Test
+	public void AssertEqualsSuccessfullResultTest(){
+		Assert.AreEquals(1, 1, "");
+		assertEquals(true, Reporter.getReporter().getResults().get(Reporter.getReporter().getResults().size()-1).wasSuccessfull());
+	}
+	
+	@Test
+	public void AssertEqualsNotSuccessfullResultTest(){
+		Assert.AreEquals(1, 2, "");
+		assertEquals(false, Reporter.getReporter().getResults().get(Reporter.getReporter().getResults().size()-1).wasSuccessfull());
+	}
+	
 }
