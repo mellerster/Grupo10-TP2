873d7724d88f7cbe3cc27468335b2f8d4171628c
Cesar Leguizamon <fabi1816@gmail.com>
2013-10-31 10:22:51 -0300

    Se actualizo el pom para que use java 7 y se cambio el nombre de la clase de tests para que maven los veo; La clase de tests se tiene que llamar 'Test*', '*Test' o '*TestCase' si no maven no los ve

--- src/test/java/TP2/TestTP2.java
+++ src/test/java/TP2/TestTP2.java
@@ -4,7 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class testTP2 {
+
+public class TestTP2 {
 	
 	@Test
 	public void ReportCountIncreaseTest(){
@@ -12,55 +13,63 @@ public class testTP2 {
 		Assert.isTrue(true,"");
 		assertEquals(beforeResults + 1, Reporter.getReporter().getResults().size());
 	}
-	
+
+
 	@Test
 	public void ReportFailCountIncreaseTest(){
 		int beforeResults = Reporter.getReporter().getFailures().size();
 		Assert.isTrue(false,"");
 		assertEquals(beforeResults + 1, Reporter.getReporter().getFailures().size());
 	}
-	
+
+
 	@Test
 	public void ReportFailCountNotIncreaseTest(){
 		int beforeResults = Reporter.getReporter().getFailures().size();
 		Assert.isTrue(true,"");
 		assertEquals(beforeResults, Reporter.getReporter().getFailures().size());
 	}
-	
+
+
 	@Test
 	public void ResultOkStateTest(){
 		Result result = new ResultOk("Description of result ok");
 		assertEquals(true, result.getState());
 	}
 
+
 	@Test
 	public void ResultFailStateTest(){
 		Result result = new ResultFail("Description of result fail");
 		assertEquals(false, result.getState());
 	}
-	
+
+
 	@Test
 	public void AssertIsTrueSuccessfullResultTest(){
 		Assert.isTrue(true, "");
 		assertEquals(true, Reporter.getReporter().getResults().get(Reporter.getReporter().getResults().size()-1).wasSuccessfull());
 	}
-	
+
+
 	@Test
 	public void AssertIsTrueNotSuccessfullResultTest(){
 		Assert.isTrue(false, "");
 		assertEquals(false, Reporter.getReporter().getResults().get(Reporter.getReporter().getResults().size()-1).wasSuccessfull());
 	}
-	
+
+
 	@Test
 	public void AssertEqualsSuccessfullResultTest(){
 		Assert.AreEquals(1, 1, "");
 		assertEquals(true, Reporter.getReporter().getResults().get(Reporter.getReporter().getResults().size()-1).wasSuccessfull());
 	}
-	
+
+
 	@Test
 	public void AssertEqualsNotSuccessfullResultTest(){
 		Assert.AreEquals(1, 2, "");
 		assertEquals(false, Reporter.getReporter().getResults().get(Reporter.getReporter().getResults().size()-1).wasSuccessfull());
 	}
-	
+
 }
