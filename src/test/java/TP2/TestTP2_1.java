29ca18b9a00ddf157f8de75d0f76082a274f73d9
mellerster <concdiego@gmail.com>
2013-11-07 00:45:48 -0300

    Ultimos 2 tests del enunciado (RegEx)

--- src/test/java/TP2/TestTP2_1.java
+++ src/test/java/TP2/TestTP2_1.java
@@ -1,5 +1,7 @@
 package TP2;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class TestTP2_1 {
@@ -82,5 +84,99 @@ public class TestTP2_1 {
 		Assert.isTrue(false, "TestError");
 	}
 	*/
+	
+	@Test
+	public void specialTests(){
+		Reporter.clear();
+		TP2.Test testMySpecialTestCase = new TP2.Test("my special test case"){
+
+			@Override
+			public void run() {
+				Assert.isTrue(true, "my special test case");
+			}
+		};
+		TP2.Test testMySpecialTestCase1 = new TP2.Test("my special test case 1"){
+
+			@Override
+			public void run() {
+				Assert.isTrue(true, "my special test case 1");
+			}
+		};
+		TP2.Test testMySpecial = new TP2.Test("my special"){
+
+			@Override
+			public void run() {
+				Assert.isTrue(true, "my special");
+			}
+		};
+		TP2.Test testATest = new TP2.Test("a test"){
+
+			@Override
+			public void run() {
+				Assert.isTrue(true, "a test");
+			}
+		};
+		TP2.TestSuite testSuite = new TP2.TestSuite(){
+
+			@Override
+			protected void init() {
+				setPattern(".*special.*");
+			}
+		};
+		testSuite.addTest(testMySpecial);
+		testSuite.addTest(testMySpecialTestCase);
+		testSuite.addTest(testMySpecialTestCase1);
+		testSuite.addTest(testATest);
+		testSuite.init();
+		testSuite.run();
+		assertEquals(3, Reporter.getReporter().getResults().size());
+	}
+	
+	@Test
+	public void UnexistentName(){
+		Reporter.clear();
+		TP2.Test testMySpecialTestCase = new TP2.Test("my special test case"){
+
+			@Override
+			public void run() {
+				Assert.isTrue(true, "my special test case");
+			}
+		};
+		TP2.Test testMySpecialTestCase1 = new TP2.Test("my special test case 1"){
+
+			@Override
+			public void run() {
+				Assert.isTrue(true, "my special test case 1");
+			}
+		};
+		TP2.Test testMySpecial = new TP2.Test("my special"){
+
+			@Override
+			public void run() {
+				Assert.isTrue(true, "my special");
+			}
+		};
+		TP2.Test testATest = new TP2.Test("a test"){
+
+			@Override
+			public void run() {
+				Assert.isTrue(true, "a test");
+			}
+		};
+		TP2.TestSuite testSuite = new TP2.TestSuite(){
+
+			@Override
+			protected void init() {
+				setPattern(".*no existe");
+			}
+		};
+		testSuite.addTest(testMySpecial);
+		testSuite.addTest(testMySpecialTestCase);
+		testSuite.addTest(testMySpecialTestCase1);
+		testSuite.addTest(testATest);
+		testSuite.init();
+		testSuite.run();
+		assertEquals(0, Reporter.getReporter().getResults().size());
+	}
 	
 }
