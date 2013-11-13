9a0e8e6c65ef3352ecdf4b510f35159cf0f9c3b3
David <david.mdq89@gmail.com>
2013-11-13 09:38:15 -0300

    Agrego tests de tag y nombre

--- src/test/java/TP2/TestsTP2_2.java
+++ src/test/java/TP2/TestsTP2_2.java
@@ -129,4 +129,141 @@ public class TestsTP2_2 {
 		TS1.run();
 		assertEquals(4, Reporter.getReporter().getResults().size());
 	}
+/*	
+	@Test
+	public void testCaseWitTagAndWithoutAName() {
+		Reporter.clear();
+		TestSuite TS1 = new TestSuite("TS1") {
+
+			@Override
+			protected void init() {
+				setPattern(".*no correr.*"); //TODO como es el negado?
+			}
+
+		};
+		TP2.Test T1 = new TP2.Test("T1") {
+
+			@Override
+			public void run() {
+				Assert.isTrue(true, "T1");
+			}
+
+		};
+		T1.setTag("SLOW");
+		TP2.Test T2 = new TP2.Test("T2") {
+
+			@Override
+			public void run() {
+				Assert.isTrue(true, "T2");
+			}
+
+		};
+		T2.setTag("FAST");
+		TP2.Test T3 = new TP2.Test("T3") {
+
+			@Override
+			public void run() {
+				Assert.isTrue(true, "T3");
+			}
+
+		};
+		T3.setTag("SLOW");
+		TP2.Test T4 = new TP2.Test("no correr") {
+
+			@Override
+			public void run() {
+				Assert.isTrue(true, "no correr");
+			}
+
+		};
+		T4.setTag("SLOW");
+
+		TS1.addTest(T1);
+		TS1.addTest(T2);
+		TS1.addTest(T3);
+		TS1.addTest(T4);
+
+		TS1.setTag("SLOW");
+		TS1.init();
+		TS1.run();
+		assertEquals(2, Reporter.getReporter().getResults().size());
+	}
+*/
+	@Test
+	public void testCaseWitTagsAndWithAName() {
+		Reporter.clear();
+		TestSuite TS1 = new TestSuite("TS1") {
+
+			@Override
+			protected void init() {
+				setPattern(".*mysql.*");
+			}
+
+		};
+		TP2.Test T1 = new TP2.Test("T1") {
+
+			@Override
+			public void run() {
+				Assert.isTrue(true, "T1");
+			}
+
+		};
+		T1.setTag("DB");
+		TP2.Test T2 = new TP2.Test("T2") {
+
+			@Override
+			public void run() {
+				Assert.isTrue(true, "T2");
+			}
+
+		};
+		T2.setTag("DB");
+		TP2.Test T3 = new TP2.Test("T3") {
+
+			@Override
+			public void run() {
+				Assert.isTrue(true, "T3");
+			}
+
+		};
+		T3.setTag("SLOW");
+		TP2.Test T4 = new TP2.Test("Test mysql number 1") {
+
+			@Override
+			public void run() {
+				Assert.isTrue(true, "Test mysql number 1");
+			}
+
+		};
+		T4.setTag("DB");
+		TP2.Test T5 = new TP2.Test("Test mysql number 2") {
+
+			@Override
+			public void run() {
+				Assert.isTrue(true, "Test mysql number 2");
+			}
+
+		};
+		T5.setTag("DB");
+		TP2.Test T6 = new TP2.Test("Test mysql number 3") {
+
+			@Override
+			public void run() {
+				Assert.isTrue(true, "Test mysql number 3");
+			}
+
+		};
+
+		TS1.addTest(T1);
+		TS1.addTest(T2);
+		TS1.addTest(T3);
+		TS1.addTest(T4);
+		TS1.addTest(T5);
+		TS1.addTest(T6);
+
+		TS1.setTag("DB");
+		TS1.init();
+		TS1.run();
+		assertEquals(2, Reporter.getReporter().getResults().size());
+	}
 }
