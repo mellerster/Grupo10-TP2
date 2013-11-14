c3d72c2cbf0a9827382acb3e437489cf100dbdef
Distribuidos <distribuidos@distribuidos-desktop.(none)>
2013-11-14 17:15:56 -0300

    Se modifico el programa principal para que use la nueva estructura de reportes

--- src/main/java/ClientTP2/Main.java
+++ src/main/java/ClientTP2/Main.java
@@ -3,8 +3,8 @@ package ClientTP2;
 public class Main {
 
 	public static void main(String[] args) {
-		TP2.Tester tester = new TP2.Tester(TP2.ReportMode.XML);
-		tester.addTests(new MyProjectTests());
+		TP2.Tester tester = new TP2.Tester( new TP2.ReportXML() );
+		tester.addTests( new MyProjectTests() );
 		tester.execute();
 	}
 
