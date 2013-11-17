49b425898c0bfdee4dfdad25f5cc7834ccff2a5b
mellerster <concdiego@gmail.com>
2013-11-17 11:06:04 -0300

    Tests de Performance
    Se agrego la funcionalidad y las pruebas "Test de performance fallido."
    y "Test de performance exitoso."

--- src/main/java/fi/uba/ar/unittester/framework/TestAnalyzer.java
+++ src/main/java/fi/uba/ar/unittester/framework/TestAnalyzer.java
@@ -9,271 +9,296 @@ import org.apache.commons.lang.Validate;
 
 /**
  * Analyzes the result of tests.
- *
+ * 
  * @author ivan.bedecarats (igbedecarats@gmail.com)
  */
 public class TestAnalyzer {
 
-    /**
-     * The total number of tests which were analyzed.
-     */
-    private int testsAnalyzed;
-    private int testWithErrors;
-    private int testWithFails;
+	/**
+	 * The total number of tests which were analyzed.
+	 */
+	private int testsAnalyzed;
+	private int testWithErrors;
+	private int testWithFails;
+	private int testWithTimeLimit;
+	/**
+	 * The name of the test currently being tested. It's never null, but it can
+	 * be empty if no test is being analyzed.
+	 */
+	private String currentTestName = "";
 
-    /**
-     * The name of the test currently being tested. It's never null, but it can
-     * be empty if no test is being analyzed.
-     */
-    private String currentTestName = "";
+	/**
+	 * Hold tests that while they were running, there were some
+	 * unhandled/unforeseen exceptions, and hence, your test case basically
+	 * crashed without executing fully. It's never null, but can be empty. Key:
+	 * Test or TestContainer name Value: All the tests that failed in the Test
+	 * or TestContainer
+	 */
+	protected Map<String, List<TestResult>> failures = new HashMap<String, List<TestResult>>();
 
-    /**
-     * Hold tests that while they were running, there were some
-     * unhandled/unforeseen exceptions, and hence, your test case basically
-     * crashed without executing fully.  It's never null, but can be empty.
-     * Key: Test or TestContainer name
-     * Value: All the tests that failed in the Test or TestContainer
-     */
-    protected Map<String, List<TestResult>> failures =
-            new HashMap<String, List<TestResult>>();
+	/**
+	 * Hold tests that was completed successfully, but the test condition of
+	 * your test criteria has failed(not what you expected it to be). It's never
+	 * null, but can be empty. Key: Test or TestContainer name Value: All the
+	 * tests that have an error in the Test or TestContainer
+	 */
+	protected Map<String, List<TestResult>> errors = new HashMap<String, List<TestResult>>();
 
-    /**
-     * Hold tests that was completed successfully, but the test condition of
-     * your test criteria has failed(not what you expected it to be). It's never
-     * null, but can be empty.
-     * Key: Test or TestContainer name
-     * Value: All the tests that have an error in the Test or TestContainer
-     */
-    protected Map<String, List<TestResult>> errors =
-            new HashMap<String, List<TestResult>>();
+	/**
+	 * Hold tests that was completed successfully and the test condition of your
+	 * test criteria is what you expected. It's never null, but can be empty.
+	 * Key: Test or TestContainer name Value: All the tests that have passed in
+	 * the Test or TestContainer
+	 */
+	protected Map<String, List<TestResult>> passed = new HashMap<String, List<TestResult>>();
 
+	protected Map<String, List<TestResult>> timeLimitError = new HashMap<String, List<TestResult>>();
+	/**
+	 * Hold all tests. It's never null, but can be empty.
+	 */
+	protected List<String> allTests = new ArrayList<String>();
 
-    /**
-     * Hold tests that was completed successfully and the test condition of your
-     * test criteria is what you expected.  It's never null, but can be empty.
-     * Key: Test or TestContainer name
-     * Value: All the tests that have passed in the Test or TestContainer
-     */
-    protected Map<String, List<TestResult>> passed =
-            new HashMap<String, List<TestResult>>();
+	/**
+	 * Gets the number of successfulTests run.
+	 * 
+	 * @return The number of successfulTests run.
+	 */
+	public int getSuccessfulTestsCount() {
+		return testsAnalyzed - failures.size() - errors.size();
+	}
 
+	/**
+	 * Returns the number of analyzed tests.
+	 * 
+	 * @return the number of analyzed tests.
+	 */
+	public int getTestsAnalyzedCount() {
+		return testsAnalyzed;
+	}
 
-    /**
-     * Hold all tests.  It's never null, but can be empty.
-     */
-    protected List<String> allTests = new ArrayList<String>();
+	/**
+	 * Returns the number of tests that have failed.
+	 * 
+	 * @return the number of tests that have failed.
+	 */
+	public int getTestsFailedCount(String testName) {
+		List<TestResult> testResults = failures.get(testName);
+		return (testResults == null) ? 0 : testResults.size();
+	}
 
+	/**
+	 * Returns the number of tests that have errors.
+	 * 
+	 * @return the number of tests that have errors.
+	 */
+	public int getTestsErrorsCount(String testName) {
+		List<TestResult> testResults = errors.get(testName);
+		return (testResults == null) ? 0 : testResults.size();
+	}
 
-    /**
-     * Gets the number of successfulTests run.
-     *
-     * @return The number of successfulTests run.
-     */
-    public int getSuccessfulTestsCount() {
-        return testsAnalyzed - failures.size() - errors.size();
-    }
+	/**
+	 * Returns the tests that have failed.
+	 * 
+	 * @return tests that have failed. It's never null, but can be empty.
+	 */
+	public Map<String, List<TestResult>> getFailures() {
+		return failures;
+	}
 
-    /**
-     * Returns the number of analyzed tests.
-     *
-     * @return the number of analyzed tests.
-     */
-    public int getTestsAnalyzedCount() {
-        return testsAnalyzed;
-    }
+	/**
+	 * Returns the tests that have errors.
+	 * 
+	 * @return tests that have errors. It's never null, but can be empty.
+	 */
+	public Map<String, List<TestResult>> getErrors() {
+		return errors;
+	}
 
-    /**
-     * Returns the number of tests that have failed.
-     *
-     * @return the number of tests that have failed.
-     */
-    public int getTestsFailedCount(String testName) {
-        List<TestResult> testResults = failures.get(testName);
-        return (testResults==null)?0:testResults.size();
-    }
+	/**
+	 * Adds a failure test.
+	 * 
+	 * @param test
+	 *            The test that has failed. It cannot be null.
+	 * @param message
+	 *            A {@link message} about the error. It cannot be null.
+	 */
+	public void addFailure(final Test test, final String message,
+			final long testTime, Throwable throwable) {
 
-    /**
-     * Returns the number of tests that have errors.
-     *
-     * @return the number of tests that have errors.
-     */
-    public int getTestsErrorsCount(String testName) {
-        List<TestResult> testResults = errors.get(testName);
-        return (testResults==null)?0:testResults.size();
-    }
+		Validate.notNull(test, "The Test cannot be null.");
+		Validate.notNull(message, "The throwable cannot be null.");
 
-    /**
-     * Returns the tests that have failed.
-     *
-     * @return tests that have failed. It's never null, but can be empty.
-     */
-    public Map<String, List<TestResult>> getFailures() {
-        return failures;
-    }
+		TestResult failure = new TestResult(test, message, testTime);
+		if (failures.containsKey(currentTestName)) {
+			failures.get(currentTestName).add(failure);
+		} else {
+			List<TestResult> failureList = new ArrayList<TestResult>();
+			failureList.add(failure);
+			testWithFails++;
+			failures.put(currentTestName, failureList);
+		}
+	}
 
-    /**
-     * Returns the tests that have errors.
-     *
-     * @return tests that have errors.  It's never null, but can be empty.
-     */
-    public Map<String, List<TestResult>> getErrors() {
-        return errors;
-    }
+	/**
+	 * Adds an error test.
+	 * 
+	 * @param test
+	 *            The error test. It cannot be null
+	 * @param message
+	 *            A {@link message} about the error. It cannot be null.
+	 */
+	public void addError(final Test test, final String message,
+			final long testTime, Throwable throwable) {
 
+		Validate.notNull(test, "The Test cannot be null.");
+		Validate.notNull(message, "The message cannot be null.");
+		Validate.notNull(testTime, "The test time cannot be null.");
+		Validate.notNull(throwable, "The throwable cannot be null.");
 
-    /**
-     * Adds a failure test.
-     *
-     * @param test    The test that has failed. It cannot be null.
-     * @param message A {@link message} about the error. It cannot be null.
-     */
-    public void addFailure(final Test test, final String message,
-                           final long testTime,Throwable throwable) {
+		currentTestName = currentTestName.isEmpty() ? test.getTestSimpleName()
+				: currentTestName;
+		TestResult error = new TestResult(test, message, testTime, throwable);
+		if (errors.containsKey(currentTestName)) {
+			errors.get(currentTestName).add(error);
+		} else {
+			List<TestResult> errorList = new ArrayList<TestResult>();
+			errorList.add(error);
+			errors.put(currentTestName, errorList);
+		}
+		testWithErrors++;
+	}
 
-        Validate.notNull(test, "The Test cannot be null.");
-        Validate.notNull(message, "The throwable cannot be null.");
+	/**
+	 * Adds a passed test.
+	 * 
+	 * @param test
+	 *            The test that has passed. It cannot be null
+	 */
+	public void addPassed(final Test test, final String message,
+			final long testTime) {
 
-        TestResult failure = new TestResult(test, message, testTime);
-        if (failures.containsKey(currentTestName)) {
-            failures.get(currentTestName).add(failure);
-        } else {
-            List<TestResult> failureList = new ArrayList<TestResult>();
-            failureList.add(failure);
-            testWithFails++;
-            failures.put(currentTestName, failureList);
-        }
-    }
+		Validate.notNull(test, "The Test cannot be null.");
 
-    /**
-     * Adds an error test.
-     *
-     * @param test    The error test. It cannot be null
-     * @param message A {@link message} about the error. It cannot be null.
-     */
-    public void addError(final Test test, final String message,
-                         final long testTime,Throwable throwable) {
+		String testSimpleName = test.getTestSimpleName();
+		currentTestName = currentTestName.isEmpty() ? testSimpleName
+				: currentTestName;
+		TestResult passedTest = new TestResult(test, message, testTime);
 
-        Validate.notNull(test, "The Test cannot be null.");
-        Validate.notNull(message, "The message cannot be null.");
-        Validate.notNull(testTime, "The test time cannot be null.");
-        Validate.notNull(throwable, "The throwable cannot be null.");
+		if (passed.containsKey(currentTestName)) {
+			passed.get(currentTestName).add(passedTest);
+		} else {
+			List<TestResult> passedList = new ArrayList<TestResult>();
+			passedList.add(passedTest);
+			passed.put(currentTestName, passedList);
+		}
+	}
 
-        currentTestName = currentTestName.isEmpty() ? test.getTestSimpleName() :
-                currentTestName;
-        TestResult error = new TestResult(test, message, testTime,throwable);
-        if (errors.containsKey(currentTestName)) {
-            errors.get(currentTestName).add(error);
-        } else {
-            List<TestResult> errorList = new ArrayList<TestResult>();
-            errorList.add(error);
-            errors.put(currentTestName, errorList);
-        }
-        testWithErrors++;
-    }
+	public void addTimeLimitError(final Test test, String message,
+			final long testTime, TestTimeLimitException testTimeLimitException) {
+		Validate.notNull(test, "The Test cannot be null.");
+		Validate.notNull(message, "The message cannot be null.");
+		Validate.notNull(testTime, "The test time cannot be null.");
+		Validate.notNull(testTimeLimitException, "The throwable cannot be null.");
+		String testSimpleName = test.getTestSimpleName();
+		currentTestName = currentTestName.isEmpty() ? testSimpleName
+				: currentTestName;
+		TestResult timeLimitErrorTest = new TestResult(test, message, testTime,testTimeLimitException);
 
-    /**
-     * Adds a passed test.
-     *
-     * @param test The test that has passed. It cannot be null
-     */
-    public void addPassed(final Test test, final String message,
-                          final long testTime) {
+		if (timeLimitError.containsKey(currentTestName)) {
+			timeLimitError.get(currentTestName).add(timeLimitErrorTest);
+		} else {
+			List<TestResult> timeErrorList = new ArrayList<TestResult>();
+			timeErrorList.add(timeLimitErrorTest);
+			timeLimitError.put(currentTestName, timeErrorList);
+		}
+		testWithTimeLimit++;
+	}
 
-        Validate.notNull(test, "The Test cannot be null.");
+	/**
+	 * Adds a test to allTest list.
+	 * 
+	 * @param test
+	 *            The test to be added. It cannot be null.
+	 */
+	public void addTest(final Test test) {
+		Validate.notNull(test, "The Test cannot be null.");
 
-        String testSimpleName = test.getTestSimpleName();
-        currentTestName = currentTestName.isEmpty() ? testSimpleName :
-                currentTestName;
-        TestResult passedTest = new TestResult(test, message, testTime);
+		currentTestName = currentTestName.isEmpty() ? test.getTestSimpleName()
+				: currentTestName;
+		if (!allTests.contains(getCurrentTestName())) {
+			allTests.add(getCurrentTestName());
+		}
+	}
 
-        if (passed.containsKey(currentTestName)) {
-            passed.get(currentTestName).add(passedTest);
-        } else {
-            List<TestResult> passedList = new ArrayList<TestResult>();
-            passedList.add(passedTest);
-            passed.put(currentTestName, passedList);
-        }
-    }
+	/**
+	 * Increment the total number of tests and add the new test to allTest list.
+	 * 
+	 * @param test
+	 *            The test to be analyzed. It cannot be null.
+	 */
+	public void start(final Test test) {
+		Validate.notNull(test, "The Test cannot be null.");
+		testsAnalyzed++;
+		addTest(test);
+	}
 
-    /**
-     * Adds a test to allTest list.
-     *
-     * @param test The test to be added. It cannot be null.
-     */
-    public void addTest(final Test test) {
-        Validate.notNull(test, "The Test cannot be null.");
+	/**
+	 * Set the name to the test that is being executed. If the test belong to a
+	 * TestContainer then testName will be TestContainerName.TestName
+	 * 
+	 * @param theCurrentTestName
+	 *            The name of the Current Test. It cannot be empty nor null.
+	 */
+	public void setCurrentTestName(final String theCurrentTestName) {
+		Validate.notEmpty(theCurrentTestName,
+				"The current test name cannot be null nor empty.");
+		currentTestName = theCurrentTestName;
+	}
 
-        currentTestName = currentTestName.isEmpty() ? test.getTestSimpleName() :
-                currentTestName;
-        if (!allTests.contains(getCurrentTestName())) {
-            allTests.add(getCurrentTestName());
-        }
-    }
+	/**
+	 * Gets the name of the test that is being executed.
+	 * 
+	 * @return The name of the test that is being executed.
+	 */
+	public String getCurrentTestName() {
+		return currentTestName;
+	}
 
-    /**
-     * Increment the total number of tests and add the new test to allTest list.
-     *
-     * @param test The test to be analyzed. It cannot be null.
-     */
-    public void start(final Test test) {
-        Validate.notNull(test, "The Test cannot be null.");
-        testsAnalyzed++;
-        addTest(test);
-    }
+	/**
+	 * Clear the currentTestName
+	 */
+	public void clearCurrentTestName() {
+		currentTestName = "";
+	}
 
-    /**
-     * Set the name to the test that is being executed. If the test belong to a
-     * TestContainer then testName will be TestContainerName.TestName
-     *
-     * @param theCurrentTestName The name of the Current Test. It cannot be empty
-     *                           nor null.
-     */
-    public void setCurrentTestName(final String theCurrentTestName) {
-        Validate.notEmpty(theCurrentTestName,
-                "The current test name cannot be null nor empty.");
-        currentTestName = theCurrentTestName;
-    }
+	/**
+	 * Returns all Tests that were executed.
+	 * 
+	 * @return All Tests that were executed.
+	 */
+	public List<String> getAllTests() {
+		return allTests;
+	}
 
-    /**
-     * Gets the name of the test that is being executed.
-     *
-     * @return The name of the test that is being executed.
-     */
-    public String getCurrentTestName() {
-        return currentTestName;
-    }
+	/**
+	 * Returns all Test that were executed and have passed.
+	 * 
+	 * @return All Test that were executed and have passed.
+	 */
+	public Map<String, List<TestResult>> getPassed() {
+		return passed;
+	}
 
-    /**
-     * Clear the currentTestName
-     */
-    public void clearCurrentTestName(){
-        currentTestName="";
-    }
+	public int getTestWithErrors() {
+		return testWithErrors;
+	}
 
-    /**
-     * Returns all Tests that were executed.
-     *
-     * @return All Tests that were executed.
-     */
-    public List<String> getAllTests() {
-        return allTests;
-    }
+	public int getTestWithFails() {
+		return testWithFails;
+	}
+	
+	public int getTestWithTimeLimitErrors(){
+		return testWithTimeLimit;
+	}
 
-    /**
-     * Returns all Test that were executed and have passed.
-     *
-     * @return All Test that were executed and have passed.
-     */
-    public Map<String, List<TestResult>> getPassed() {
-        return passed;
-    }
-
-
-    public int getTestWithErrors() {
-        return testWithErrors;
-    }
-
-    public int getTestWithFails() {
-        return testWithFails;
-    }
 }
--- src/main/java/fi/uba/ar/unittester/framework/UnitTest.java
+++ src/main/java/fi/uba/ar/unittester/framework/UnitTest.java
@@ -4,49 +4,64 @@ import java.util.Map;
 
 /**
  * Runs a Unit Test.
- *
+ * 
  * @author ivan.bedecarats (igbedecarats@gmail.com)
  */
 
 public abstract class UnitTest extends Test {
+	long timeToRun;
 
+	/**
+	 * Runs test method and save the results in analyzer.
+	 * 
+	 * @param analyzer
+	 * @param tags
+	 */
+	@Override
+	public final void run(final TestAnalyzer analyzer,
+			final Map<String, Object> context, final String... tags) {
+		analyzer.start(this);
+		long startTime = 0;
+		long endTime;
+		long totalTime = 0;
+		try {
+			setup(context);
+			startTime = System.nanoTime();
+			test(context);
+			endTime = System.nanoTime();
+			totalTime = endTime - startTime;
+			if (totalTime > getLimitTime()) {
+				throw new TestTimeLimitException("Limit:" + timeToRun
+						+ ", Time: " + totalTime);
+			}
+			analyzer.addPassed(this, "This test was succesfull", totalTime);
+		} catch (AssertionFailError e) {
+			endTime = System.nanoTime();
+			totalTime = endTime - startTime;
+			analyzer.addError(this, e.getAssertionName(), totalTime, e);
+		} catch (TestTimeLimitException e) {
+			analyzer.addTimeLimitError(this, e.getMessage(), totalTime, e);
+		} catch (Throwable e) {
+			endTime = System.nanoTime();
+			totalTime = endTime - startTime;
+			analyzer.addFailure(this, e.getClass().toString(), totalTime, e);
+		}
+		tearDown(context);
+	}
 
-    /**
-     * Runs test method and save the results in analyzer.
-     *
-     * @param analyzer
-     * @param tags
-     */
-    @Override
-    public final void run(final TestAnalyzer analyzer,
-                          final Map<String, Object> context,final String... tags) {
-        analyzer.start(this);
-        long startTime = 0;
-        try {
-            setup(context);
-            startTime = System.nanoTime();
-            test(context);
-            long endTime = System.nanoTime();
-            long totalTime = endTime - startTime;
-            analyzer.addPassed(this, "This test was succesfull", totalTime);
-        } catch (AssertionFailError e) {
-            long endTime = System.nanoTime();
-            long totalTime = endTime - startTime;
-            analyzer.addError(this, e.getAssertionName(), totalTime, e);
-        } catch (Throwable e) {
-            long endTime = System.nanoTime();
-            long totalTime = endTime - startTime;
-            analyzer.addFailure(this, e.getClass().toString(), totalTime, e);
-        }
-        tearDown(context);
-    }
+	/**
+	 * Defines the test to be run.
+	 * 
+	 * @param context
+	 */
+	public abstract void test(Map<String, Object> context);
 
+	public void limitTo(long timeToRun) {
+		this.timeToRun = timeToRun;
+	}
 
-    /**
-     * Defines the test to be run.
-     *
-     * @param context
-     */
-    public abstract void test(Map<String, Object> context);
+	public long getLimitTime() {
+		return timeToRun;
+	}
 
 }
