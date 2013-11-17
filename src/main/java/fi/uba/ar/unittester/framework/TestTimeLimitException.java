d963bdb8832798159caf908dae49520f6d609dbd
David <david.mdq89@gmail.com>
2013-11-17 16:21:29 -0300

    Revert "Revert "Tests de Performance""
    This reverts commit 1882cd1295d69e6cc286aa18fa0050cdee999602.

---
+++ src/main/java/fi/uba/ar/unittester/framework/TestTimeLimitException.java
@@ -0,0 +1,12 @@
+package fi.uba.ar.unittester.framework;
+
+@SuppressWarnings("serial")
+public class TestTimeLimitException extends RuntimeException {
+	public TestTimeLimitException(){
+		super();
+	}
+	
+	public TestTimeLimitException(String message){
+		super(message);
+	}
+}
---
+++ src/test/java/fi/uba/ar/unittester/framework/PerformanceTestFailed.java
@@ -0,0 +1,38 @@
+package fi.uba.ar.unittester.framework;
+
+import java.util.Map;
+
+import org.junit.Before;
+
+public class PerformanceTestFailed extends UnitTest {
+
+    private TestRunner runner;
+
+    @Before
+    public void setUp() {
+        runner = new TestRunner();
+        limitTo(65741574);
+    }
+
+    @org.junit.Test
+    public void execute_Test() {
+        runner.addTest(this);
+        runner.execute();
+        TestAnalyzer analyzer = runner.getAnalyzer();
+        Assert.assertEquals(analyzer.getTestWithTimeLimitErrors(),1);
+    }
+
+
+
+    @Override
+    public void test(Map<String, Object> context) {
+        try {
+			Thread.sleep(100);
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+        Assert.assertTrue(true);
+    }
+
+}
