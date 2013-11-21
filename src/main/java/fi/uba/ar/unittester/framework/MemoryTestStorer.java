d607d41d5e25db9d954cb28896e96850541a71bc
mellerster <concdiego@gmail.com>
2013-11-20 23:04:29 -0300

    Comentarios en MemoryTestStorer
    Que faltaba en el anterior

--- src/main/java/fi/uba/ar/unittester/framework/MemoryTestStorer.java
+++ src/main/java/fi/uba/ar/unittester/framework/MemoryTestStorer.java
@@ -3,8 +3,13 @@ package fi.uba.ar.unittester.framework;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A Test Storer that stores the tests in a ArrayList
+ * 
+ */
+
 public class MemoryTestStorer implements TestStorer {
-	
+
 	private static List<Test> tests;
 
 	public MemoryTestStorer() {
@@ -13,10 +18,17 @@ public class MemoryTestStorer implements TestStorer {
 		}
 	}
 
+	/**
+	 * Adds the test to the ArrayList, it doesn't matter if it is already
+	 */
 	public void store(Test test) {
 		tests.add(test);
 	}
 
+	/**
+	 * Converts the ArrayList of Test to a ArrayList of String with the
+	 * names of the tests
+	 */
 	public List<String> getStored() {
 		List<String> testsNames = new ArrayList<String>();
 		for (Test test : tests) {
@@ -25,6 +37,9 @@ public class MemoryTestStorer implements TestStorer {
 		return testsNames;
 	}
 
+	/**
+	 * recreate the ArrayList of Test
+	 */
 	public void reset() {
 		tests = new ArrayList<Test>();
 	}
